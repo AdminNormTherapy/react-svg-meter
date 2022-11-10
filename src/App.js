e065348575537a4555400737d4ede474c5c5b5ff
AdminNormTherapy <111477333+AdminNormTherapy@users.noreply.github.com>
2022-11-10 08:53:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,23 +33,23 @@ class App extends Component {
         <div className="header">Main Output Display</div>
         <div className="group">
           <div className="title">Word Deviation and Coherence</div>
-          <div className="meter"><Meter percent={.2} color={'#FFEB3B'} rounded={false}/></div>
-          <div className="meter"><Meter percent={.6} color={'#FFEB3B'} rounded={false}/></div>
+          <div className="meter"><Meter percent={.2} rounded={false}/></div>
+          <div className="meter"><Meter percent={.6} rounded={false}/></div>
         </div>
         <div className="group">
           <div className="title">Face Vector Deviation and Coherence</div>
-          <div className="meter"><Meter percent={this.state.r1} color={'#FFEB3B'} animate={true} rounded={false}/></div>
-          <div className="meter"><Meter percent={this.state.r2} color={'#FFEB3B'} animate={true}/></div>
+          <div className="meter"><Meter percent={this.state.r1} animate={true} rounded={false}/></div>
+          <div className="meter"><Meter percent={this.state.r2} animate={true} rounded={false}//></div>
         </div>
         <div className="group">
           <div className="title">Spectrum Sample R and L</div>
-          <div className="meter"><Meter percent={.4} color={'#FFEB3B'} rounded={false}/></div>
-          <div className="meter"><Meter percent={.9} color={'#FFEB3B'}/></div>
+          <div className="meter"><Meter percent={.4} rounded={false}/></div>
+          <div className="meter"><Meter percent={.9} rounded={false}//></div>
         </div>
         <div className="group">
           <div className="title">Overall Deviation</div>
-          <div className="meter"><Meter percent={this.state.r3*this.state.r1} color={'#FFEB3B'} animate={true} width={250} height={17} rounded={false}/></div>
-          <div className="meter"><Meter percent={this.state.r3*this.state.r2} color={'#FFEB3B'} animate={true}  width={250} height={17} /></div>
+          <div className="meter"><Meter percent={this.state.r3*this.state.r1} animate={true} width={250} height={17} rounded={false}/></div>
+          <div className="meter"><Meter percent={this.state.r3*this.state.r2} animate={true}  width={250} height={17} rounded={false}//></div>
         </div>
       </div>
     );
