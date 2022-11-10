ec3bd66728ab65eff8e699877dc065fc94e0557c
AdminNormTherapy <111477333+AdminNormTherapy@users.noreply.github.com>
2022-11-10 08:43:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ class App extends Component {
     super(props);
     this.state = {
       r1: .7,
-      r2: .3
+      r2: .3,
+      r3: .5
     };
   }
   componentDidMount() {
     this.animatedMeter = setInterval(
       () => this.animate(),
-      1000
+      367
     );
   }
   componentWillUnmount() {
@@ -22,37 +23,38 @@ class App extends Component {
   animate(){
     this.setState({
       r1: Math.random(),
-      r2: Math.random()
+      r2: Math.random(),
+      r3: Math.random()
     })
   }
   render() {
     return (
       <div className="App">
-        <div className="header">meters for days</div>
+        <div className="header">Main Output Display</div>
         <div className="group">
-          <div className="title">rounded</div>
-          <div className="meter"><Meter percent={.3}/></div>
-          <div className="meter"><Meter percent={.8}/></div>
+          <div className="title">Spectrum Sample R and L</div>
+          <div className="meter"><Meter percent={.3} color={#FFEB3B} /></div>
+          <div className="meter"><Meter percent={.8} color={#FFEB3B} /></div>
         </div>
         <div className="group">
-          <div className="title">square</div>
-          <div className="meter"><Meter percent={.2} rounded={false}/></div>
-          <div className="meter"><Meter percent={.6} rounded={false}/></div>
+          <div className="title">Word Deviation and Coherence</div>
+          <div className="meter"><Meter percent={.2} color={#FFEB3B} rounded={false}/></div>
+          <div className="meter"><Meter percent={.6} color={#FFEB3B} rounded={false}/></div>
         </div>
         <div className="group">
-          <div className="title">animated</div>
-          <div className="meter"><Meter percent={this.state.r1} animate={true} rounded={false}/></div>
-          <div className="meter"><Meter percent={this.state.r2} animate={true}/></div>
+          <div className="title">Face Vector Deviation and Coherence</div>
+          <div className="meter"><Meter percent={this.state.r1} color={#FFEB3B} animate={true} rounded={false}/></div>
+          <div className="meter"><Meter percent={this.state.r2} color={#FFEB3B} animate={true}/></div>
         </div>
         <div className="group">
           <div className="title">colorful</div>
-          <div className="meter"><Meter percent={.4} color={'#F44336'} rounded={false}/></div>
+          <div className="meter"><Meter percent={.4} color={'#FFEB3B'} rounded={false}/></div>
           <div className="meter"><Meter percent={.9} color={'#FFEB3B'}/></div>
         </div>
         <div className="group">
-          <div className="title">bigger</div>
-          <div className="meter"><Meter percent={.4} width={250} height={17} rounded={false}/></div>
-          <div className="meter"><Meter percent={.9} width={250} height={17} /></div>
+          <div className="title">Overall Deviation</div>
+          <div className="meter"><Meter percent={this.state.r3*this.state.r1} color={#FFEB3B} animate={true} width={250} height={17} rounded={false}/></div>
+          <div className="meter"><Meter percent={this.state.r3*this.state.r2} color={#FFEB3B} animate={true}  width={250} height={17} /></div>
         </div>
       </div>
     );
